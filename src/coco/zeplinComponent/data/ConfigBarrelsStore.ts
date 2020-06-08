90f00d7d781991ba2ca1a3020e198aea71b539b0
Onur Alsaran <onur@zeplin.io>
2020-06-08 13:04:48 +0300

    Remove duplicate barrel store method calls

--- src/coco/zeplinComponent/data/ConfigBarrelsStore.ts
+++ src/coco/zeplinComponent/data/ConfigBarrelsStore.ts
@@ -4,7 +4,7 @@ import BarrelDetailsStoreProvider from "../../../common/domain/zeplinComponent/d
 import BarrelType from "../../../common/domain/barrel/BarrelType";
 import BarrelDetails from "../../../common/domain/zeplinComponent/model/BarrelDetails";
 import Result from "../../../common/domain/store/Result";
-import { flatten } from "../../../common/general/arrayUtil";
+import { flatten, isFirstOccurence } from "../../../common/general/arrayUtil";
 import BarrelError from "../../../common/domain/zeplinComponent/model/BarrelError";
 import { isBarrelIdFormatValid } from "../../../common/domain/barrel/util/barrelUtil";
 
@@ -13,8 +13,8 @@ export default class ConfigBarrelsStore implements Store<BarrelDetails[], Barrel
 
     public get = async (): Promise<Result<BarrelDetails[], BarrelError>> => {
         const config = getConfig(this.path);
-        const projects = config.getProjects().filter(isBarrelIdFormatValid);
-        const styleguides = config.getStyleguides().filter(isBarrelIdFormatValid);
+        const projects = config.getProjects().filter(isFirstOccurence).filter(isBarrelIdFormatValid);
+        const styleguides = config.getStyleguides().filter(isFirstOccurence).filter(isBarrelIdFormatValid);
 
         const results = await Promise.all([
             ...projects.map(project => BarrelDetailsStoreProvider.get(project, BarrelType.Project).get()),
