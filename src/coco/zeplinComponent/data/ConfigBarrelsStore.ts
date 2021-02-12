4c38783d05397d2a9d5cead158951588d60ad246
Onur Alsaran <onur@zeplin.io>
2021-02-12 11:46:38 +0300

    Use Config barrels method on ConfigBarrelsStore

--- src/coco/zeplinComponent/data/ConfigBarrelsStore.ts
+++ src/coco/zeplinComponent/data/ConfigBarrelsStore.ts
@@ -1,25 +1,20 @@
 import Store from "../../../common/domain/store/Store";
 import { getConfig } from "../../config/util/configUtil";
 import BarrelDetailsStoreProvider from "../../../common/domain/zeplinComponent/data/BarrelDetailsStoreProvider";
-import BarrelType from "../../../common/domain/barrel/BarrelType";
 import BarrelDetails from "../../../common/domain/zeplinComponent/model/BarrelDetails";
 import Result from "../../../common/domain/store/Result";
-import { flatten, isFirstOccurence } from "../../../common/general/arrayUtil";
+import { flatten } from "../../../common/general/arrayUtil";
 import BarrelError from "../../../common/domain/zeplinComponent/model/BarrelError";
-import { isBarrelIdFormatValid } from "../../../common/domain/barrel/util/barrelUtil";
 
 export default class ConfigBarrelsStore implements Store<BarrelDetails[], BarrelError> {
     public constructor(private path: string) { }
 
     public get = async (): Promise<Result<BarrelDetails[], BarrelError>> => {
         const config = getConfig(this.path);
-        const projects = config.getProjects().filter(isFirstOccurence).filter(isBarrelIdFormatValid);
-        const styleguides = config.getStyleguides().filter(isFirstOccurence).filter(isBarrelIdFormatValid);
-
-        const results = await Promise.all([
-            ...projects.map(project => BarrelDetailsStoreProvider.get(project, BarrelType.Project).get()),
-            ...styleguides.map(styleguide => BarrelDetailsStoreProvider.get(styleguide, BarrelType.Styleguide).get())
-        ]);
+        const barrels = config.getValidBarrelsWithTypes();
+        const results = await Promise.all(
+            barrels.map(({ id, type }) => BarrelDetailsStoreProvider.get(id, type).get())
+        );
 
         return {
             data: results.filter(result => result.data).map(result => result.data!),
